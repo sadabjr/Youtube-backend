e4f0a814ecb9e675933ba9e7402333367614a180
Sadab Ali <sadabjr047@gmail.com>
2024-05-07 23:53:45 +0530

    utils api error and api response setup

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,15 +1,19 @@
-// const asyncHandler = () =>{};
+const asyncHandler = (requestHandler) =>{
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+};
 
 export {asyncHandler}
 
-const asyncHandler = (fn) => async(req, res, next) => {
-    try{
-
-    }catch(error){
-        res.status(error.code || 5000).json({
-            success: false,
-            message: error.message
-        })
-        console.log(error)
-    }
-}
+// const asyncHandler = (fn) => async(req, res, next) => {
+//     try{
+//         await fn(req, res, next)
+//     }catch(error){
+//         res.status(error.code || 5000).json({
+//             success: false,
+//             message: error.message
+//         })
+//         console.log(error)
+//     }
+// }
